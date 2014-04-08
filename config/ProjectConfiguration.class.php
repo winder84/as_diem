f3fba29e9e08882f507dfbcccb0529add5da5839
rustam <rustam@nodasoft.com>
2014-04-08 09:30:48 +0400

    new

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '/home/s/stokkmvru/diem50/dmCorePlugin/lib/core/dm.php';
+dm::start();
+
+class ProjectConfiguration extends dmProjectConfiguration
+{
+
+  public function setup()
+  {
+    parent::setup();
+    
+    $this->enablePlugins(array(
+      // add plugins you want to enable here
+    ));
+
+    $this->setWebDir(sfConfig::get('sf_root_dir').'/public_html');
+  }
+  
+}
