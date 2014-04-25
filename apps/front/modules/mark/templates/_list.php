5b5854f079f15993af1a1033a4ce14e2eb6566f6
rustam <rustam@nodasoft.com>
2014-04-25 17:33:47 +0400

    плашка фирмы

--- apps/front/modules/mark/templates/_list.php
+++ apps/front/modules/mark/templates/_list.php
@@ -1,18 +1,23 @@
 <?php // Vars: $markPager
 
-echo $markPager->renderNavigationTop();
+//echo $markPager->renderNavigationTop();
 
-echo _open('ul.elements');
-
-foreach ($markPager as $mark)
-{
-  echo _open('li.element');
-
-    echo _link($mark);
-
-  echo _close('li');
-}
-
+echo _open('ul.header_categories_menu');
+	echo _open('div.mainCategoryTitle');
+		echo _media('made_in.jpg')->width(90);
+		echo _tag('p', 'Производители');
+	echo _close('div');
+	echo _open('div.cats');
+		foreach ($markPager as $key => $mark)
+		{
+			if ($key <= 7) {
+				echo _open('li');
+					echo _link($mark)->text(_media($mark->Image)->size(100, 40));
+					echo _tag('p.category_name_link', $mark);
+				echo _close('li');
+			}
+		}
+	echo _close('div');
 echo _close('ul');
 
-echo $markPager->renderNavigationBottom();
+//echo $markPager->renderNavigationBottom();
