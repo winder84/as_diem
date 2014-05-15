7759950b944202a721cdfee36b4bc773910f292c
rustam <rustam@nodasoft.com>
2014-05-15 17:21:33 +0400

    блоки оборудования

--- apps/front/modules/product/templates/_listByCategory.php
+++ apps/front/modules/product/templates/_listByCategory.php
@@ -6,12 +6,12 @@ echo _open('ul.productListUl');
 
 foreach ($productPager as $product)
 {
-  echo _open('li.element');
+	echo _open('li.element');
 
-	echo _media($product->Image)->size(190, 130);
-    echo _link($product);
+	echo _link($product)->text(_media($product->Image)->size(190, 130)->method('fit'));
+	echo _link($product);
 
-  echo _close('li');
+	echo _close('li');
 }
 
 echo _close('ul');
