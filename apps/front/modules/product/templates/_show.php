023a278bfa9ff310b062bc7241a2156d05889521
rustam <rustam@nodasoft.com>
2014-05-16 11:49:10 +0400

    начало страницы продукта

--- apps/front/modules/product/templates/_show.php
+++ apps/front/modules/product/templates/_show.php
@@ -1,3 +1,16 @@
 <?php // Vars: $product
 
-echo $product;
+echo _open('div.productShowDiv');
+	echo _open('div.productShowHead');
+		echo _open('div.productShowImg');
+			echo _media($product->Image)->width(500);
+		echo _close('div');
+		echo _open('div.productShowTD');
+			echo _tag('p.productShowTitle', $product);
+			echo _tag('p.productShowPrevdesc', $product->prevdescription);
+		echo _close('div');
+	echo _close('div');
+	echo _open('div.productShowBody');
+		echo _tag('p.productShowDescription', $product->description);
+	echo _close('div');
+echo _close('div');
