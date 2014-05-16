bde34516fe3cc63a4f897aaf84948d73006f7f11
rustam <rustam@nodasoft.com>
2014-05-16 14:50:53 +0400

    productShow

--- apps/front/modules/product/templates/_show.php
+++ apps/front/modules/product/templates/_show.php
@@ -1,16 +1,37 @@
 <?php // Vars: $product
+foreach ($product->Specification as $spec) {
+	$SpecArray[] = $spec;
+}
 
 echo _open('div.productShowDiv');
 	echo _open('div.productShowHead');
 		echo _open('div.productShowImg');
-			echo _media($product->Image)->width(500);
+			echo _media($product->Image)->size(500, 300)->method('fit');
 		echo _close('div');
+		echo _tag('div.productVertDelimer');
 		echo _open('div.productShowTD');
 			echo _tag('p.productShowTitle', $product);
 			echo _tag('p.productShowPrevdesc', $product->prevdescription);
 		echo _close('div');
 	echo _close('div');
+//	echo _tag('div.productHorDelimer');
 	echo _open('div.productShowBody');
 		echo _tag('p.productShowDescription', $product->description);
+		if (!empty($product->cost)) {
+			echo _tag('p.productShowCost', 'Цена: ' . $product->cost . ' руб.');
+		}
 	echo _close('div');
+
+	if(!empty($SpecArray)) {
+		echo _tag('div.productHorDelimer');
+		echo _tag('p.specTableTitle', 'Характеристики');
+		echo _open('table.specTable');
+		foreach ($SpecArray as $spec) {
+			echo _open('tr');
+			echo _tag('td', $spec);
+			echo _tag('td', $spec->Specificationlink[0]->value . ' ' . $spec->measure);
+			echo _close('tr');
+		}
+		echo _close('table');
+	}
 echo _close('div');
