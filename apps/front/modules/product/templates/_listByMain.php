be5bceff6dc4082b94557b2cca66dc83f64dba15
rustam <rustam@nodasoft.com>
2014-08-06 14:11:50 +0400

    схема и убрал характеристики

---
+++ apps/front/modules/product/templates/_listByMain.php
@@ -0,0 +1,21 @@
+<?php // Vars: $productPager
+
+//echo $productPager->renderNavigationTop();
+
+echo _open('ul.productListUl');
+
+foreach ($productPager as $product)
+{
+	if ($product->isonmain) {
+		echo _open('li.element');
+
+		echo _link($product)->text(_media($product->Image)->size(200, 130)->method('fit'));
+		echo _tag('div.productElementTitle', _link($product));
+
+		echo _close('li');
+	}
+}
+
+echo _close('ul');
+
+//echo $productPager->renderNavigationBottom();
--- apps/front/modules/product/templates/_listBySlider.php
+++ apps/front/modules/product/templates/_listBySlider.php
@@ -1,15 +1,14 @@
 <?php // Vars: $productPager
 
 //echo $productPager->renderNavigationTop();
-echo _open('ul.productListUl');
+echo _open('ul');
 
 foreach ($productPager as $product)
 {
 	if ($product->isonslider) {
 		echo _open('li.element');
 
-		echo _link($product)->text(_media($product->Image)->size(200, 130)->method('fit'));
-		echo _tag('div.productElementTitle', _link($product));
+		echo _link($product);
 
 		echo _close('li');
 	}
